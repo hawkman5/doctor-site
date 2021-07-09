0ec73a6d35e1d65d5ede878559d42bfc84f1a5b5
Zachary Maes <zachary.maes@gmail.com>
2021-07-09 19:29:04 -0400

    made js animations for fade in of navbar

--- app.js
+++ app.js
@@ -1,10 +1,22 @@
 const navSlide = () => {
-    const burger = document.querySelector(".burger");
-    const nav = document.querySelector(".navbar-nav");
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.navbar-nav');
+    const navLinks = document.querySelectorAll('.navbar-nav li');
 
-    burger.addEventListener("click", () => {
-        nav.classList.toggle("nav-active");
-    }
+    
+    burger.addEventListener('click', () => {
+        //Toggle Nav
+        nav.classList.toggle('navbar-active');
+
+        //Animate Links
+        navLinks.forEach((link, index) => {
+            if(link.style.animation){
+                link.style.animation = ''
+            }else {
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`;
+        }2
+    });
+    });
 }
 
 navSlide();
