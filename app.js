c885f3981c9a9edc9de67a1cd03f395ffe20dd6a
Zachary Maes <zachary.maes@gmail.com>
2021-07-09 19:52:31 -0400

    finished the navbar

--- app.js
+++ app.js
@@ -14,8 +14,10 @@ const navSlide = () => {
                 link.style.animation = ''
             }else {
                 link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`;
-        }2
-    });
+            }
+        });
+        //Burger Animation
+        burger.classList.toggle('toggle');
     });
 }
 
