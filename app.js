9c14b70bd58ce8cb77197eb3c1309f284089717a
Zachary Maes <zachary.maes@gmail.com>
2021-10-27 15:37:40 -0700

    added a beach photo to files to use later, tried to write js for er wait time but it does not work yet, put js links i used in the the comments on app.js

--- app.js
+++ app.js
@@ -22,3 +22,40 @@ const navSlide = () => {
 }
 
 navSlide();
+
+
+// My first fetch function
+
+// fetch('https://www.sebastianrivermedical.org/data/erwait?_format=json').then(function (response) {
+//         return response.json();
+//     }).then(function (obj) {
+//         console.log(obj);
+//     }).catch(function (error) {
+//         console.error('Something went wrong with the fetching');
+//         console.error(error);
+//     })
+
+// ------------------------
+
+// async fetch from stack overflow
+// https://stackoverflow.com/questions/48474970/saving-fetched-json-into-variable/48475017
+
+// new code from youtube video
+// https://www.youtube.com/watch?v=Yp9KIcSKTNo
+
+async function getData() {
+    return (await fetch("https://www.sebastianrivermedical.org/data/erwait?_format=json")).json();
+  }
+  
+  document.addEventListener("DOMContentLoaded", async () => {
+    let erWait = [];
+
+    try {
+        erWait = await getData();
+    } catch (e) {
+        console.log("Error!");
+        console.log(e);
+    }
+
+    console.log(erWait);
+  });
